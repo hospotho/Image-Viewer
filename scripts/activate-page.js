5ab2d22a8f09f303097594ceed0df371b3a37569
hospotho <a155268747@gmail.com>
2022-07-18 17:01:03 +0800

    update simpleUnlazyImage to choose biggest img

--- scripts/activate-page.js
+++ scripts/activate-page.js
@@ -10,27 +10,48 @@
     return
   }
 
-  function simpleUnlazyImage() {
-    document.documentElement.classList.add('has-unlazy')
-    for (const img of document.querySelectorAll('img[loading]')) {
-      img.loading = 'eager'
-    }
-    var lazyName = ''
-    const reg = /^(?:https?:\/)?\/.+/
+  function getImageSize(src) {
+    return new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => resolve(img.naturalWidth)
+      img.onerror = reject
+      img.src = src
+    })
+  }
+
+  async function simpleUnlazyImage() {
     const imgList = document.querySelectorAll('img')
+    const countClass = [...document.documentElement.classList].find(x => x.startsWith('unlazy-count-'))
+    if (imgList.length === parseInt(countClass?.substring(13))) return
+    document.documentElement.classList.remove(countClass)
+    document.documentElement.classList.add(`unlazy-count-${imgList.length}`)
+
+    for (const img of imgList) img.loading = 'eager'
+    var lazyName = ''
+    var mult = false
+    const reg = /^(?:https?:\/)?\/.+/
+    const multReg = /(?:https?:\/)?\/\S+\.[a-zA-Z]{3,4}/g
     const maxCheck = imgList.length - Math.min(parseInt(imgList.length / 5 + 5), imgList.length)
     top: for (let i = imgList.length - 1; i > maxCheck; i--) {
       for (const attr of imgList[i].attributes) {
-        if (attr.name !== 'src' && reg.test(attr.value)) {
-          lazyName = attr.name
-          break top
-        }
+        if (attr.name === 'src' || !reg.test(attr.value)) continue
+        lazyName = attr.name
+
+        const match = [...attr.value.matchAll(multReg)]
+        if (match.length === 1) break top
+        const first = match[0][0]
+        const last = match[match.length - 1][0]
+        const [firstSize, LastSize] = await Promise.all([getImageSize(first), getImageSize(last)])
+        mult = LastSize > firstSize
+        break top
       }
     }
     if (!lazyName) return
     console.log(`Unlazy img with ${lazyName} attr`)
-    for (const img of document.querySelectorAll(`img[${lazyName}]`)) {
-      img.src = img.getAttribute(lazyName).split(' ')[0]
+    const lazyImage = document.querySelectorAll(`img[${lazyName}]`)
+    const getLazyURL = mult ? match => match.slice(-1) : match => match[0]
+    for (const img of lazyImage) {
+      img.src = getLazyURL([...img.getAttribute(lazyName).matchAll(multReg)])
     }
   }
 
@@ -61,7 +82,7 @@
     return uniqueImageUrls
   }
 
-  chrome.runtime.sendMessage('get_options', res => {
+  chrome.runtime.sendMessage('get_options', async res => {
     if (!res) return
     var {options} = res
     options.closeButton = true
@@ -71,9 +92,7 @@
       return
     }
 
-    if (!document.documentElement.classList.contains('has-unlazy')) {
-      simpleUnlazyImage()
-    }
+    await simpleUnlazyImage()
 
     var uniqueImageUrls = getImageList(options)
     if (uniqueImageUrls.length === 0) return
