d14d7af114b66a399d7fdee6dc70ee35789db149
hospotho <a155268747@gmail.com>
2022-07-18 18:05:29 +0800

    remove unnecessary check

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -8,12 +8,12 @@
     options.minWidth = 0
     options.minHeight = 0
 
-    var images = document.querySelectorAll('body img[src="' + location.href + '"]')
-    if (images.length && images[0].src == location.href) {
-      images[0].style.display = 'none'
+    var image = document.querySelector(`body img[src='${location.href}']`)
+    if (image) {
+      image.style.display = 'none'
 
       chrome.runtime.sendMessage('load_script', res => {
-        imageViewer([images[0].src], options)
+        imageViewer([image.src], options)
       })
     }
   })
