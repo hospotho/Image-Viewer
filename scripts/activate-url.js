8e16bedb4f67bcacb0cb4fcf2c2289ae72dd4813
hospotho <a155268747@gmail.com>
2022-08-10 00:26:06 +0800

    reduce loading cost in not img tab

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -1,20 +1,19 @@
 ;(function () {
   'use strict'
 
-  chrome.runtime.sendMessage('get_options', res => {
-    if (!res) return
-    var {options} = res
-    options.closeButton = false
-    options.minWidth = 0
-    options.minHeight = 0
-
-    var image = document.querySelector(`body img[src='${location.href}']`)
-    if (image) {
-      image.style.display = 'none'
-
+  var image = document.querySelector(`body img[src='${location.href}']`)
+  if (image) {
+    chrome.runtime.sendMessage('get_options', res => {
+      if (!res) return
+      var {options} = res
+      options.closeButton = false
+      options.minWidth = 0
+      options.minHeight = 0
+      
       chrome.runtime.sendMessage('load_script', res => {
+        image.style.display = 'none'
         imageViewer([image.src], options)
       })
-    }
-  })
+    })
+  }
 })()
