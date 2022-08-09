80ad11b837588931c5ec36ead6a041293457d1fe
hospotho <a155268747@gmail.com>
2022-08-10 04:34:22 +0800

    use await as other script does

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -1,19 +1,15 @@
-;(function () {
+;(async function () {
   'use strict'
 
   var image = document.querySelector(`body img[src='${location.href}']`)
-  if (image) {
-    chrome.runtime.sendMessage('get_options', res => {
-      if (!res) return
-      var {options} = res
-      options.closeButton = false
-      options.minWidth = 0
-      options.minHeight = 0
-      
-      chrome.runtime.sendMessage('load_script', res => {
-        image.style.display = 'none'
-        imageViewer([image.src], options)
-      })
-    })
-  }
+  if (!image) return
+  
+  var {options} = await chrome.runtime.sendMessage('get_options')
+  options.closeButton = false
+  options.minWidth = 0
+  options.minHeight = 0
+
+  await chrome.runtime.sendMessage('load_script')
+  image.style.display = 'none'
+  imageViewer([image.src], options)
 })()
