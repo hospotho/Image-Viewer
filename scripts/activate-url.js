16b71dd3e3bad5bce2ace05f43153228339fa0ad
hospotho <a155268747@gmail.com>
2023-02-13 02:43:10 +0800

    remove else branch

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -11,47 +11,49 @@
     await chrome.runtime.sendMessage('load_script')
     image.style.display = 'none'
     imageViewer([image.src], options)
-  } else {
-    if (document.visibilityState === 'visible') {
-      if (document.readyState === 'complete') {
-        console.log('Init content script.')
-        chrome.runtime.sendMessage('load_worker')
-      } else {
-        window.addEventListener('load', () => {
-          console.log('Init content script.')
-          chrome.runtime.sendMessage('load_worker')
-        })
-      }
-      return
-    }
-    console.log('Waiting user to view the page.')
-    const handleEvent = () => {
-      document.removeEventListener('visibilitychange', handleEvent)
-      window.removeEventListener('focus', handleEvent)
-      if (document.readyState === 'complete') {
-        console.log('Init content script.')
-        chrome.runtime.sendMessage('load_worker')
-      } else {
-        window.addEventListener('load', () => {
-          console.log('Init content script.')
-          chrome.runtime.sendMessage('load_worker')
-        })
-      }
-    }
-    document.addEventListener('visibilitychange', handleEvent)
-    window.addEventListener('focus', handleEvent)
+    return
+  }
 
-    const observer = new MutationObserver(mutations => {
-      outer: for (const mutation of mutations) {
-        for (const node of mutation.addedNodes) {
-          if (node.nodeType === 1 && node.ownerDocument.querySelector('iframe')) {
-            chrome.runtime.sendMessage('load_worker')
-            break outer
-          }
+  if (document.visibilityState === 'visible') {
+    if (document.readyState === 'complete') {
+      console.log('Init content script.')
+      chrome.runtime.sendMessage('load_worker')
+    } else {
+      window.addEventListener('load', () => {
+        console.log('Init content script.')
+        chrome.runtime.sendMessage('load_worker')
+      })
+    }
+    return
+  }
+  console.log('Waiting user to view the page.')
+  
+  const handleEvent = () => {
+    document.removeEventListener('visibilitychange', handleEvent)
+    window.removeEventListener('focus', handleEvent)
+    if (document.readyState === 'complete') {
+      console.log('Init content script.')
+      chrome.runtime.sendMessage('load_worker')
+    } else {
+      window.addEventListener('load', () => {
+        console.log('Init content script.')
+        chrome.runtime.sendMessage('load_worker')
+      })
+    }
+  }
+  document.addEventListener('visibilitychange', handleEvent)
+  window.addEventListener('focus', handleEvent)
+
+  const observer = new MutationObserver(mutations => {
+    outer: for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (node.nodeType === 1 && node.ownerDocument.querySelector('iframe')) {
+          chrome.runtime.sendMessage('load_worker')
+          break outer
         }
       }
-    })
+    }
+  })
 
-    observer.observe(document, {childList: true, subtree: true})
-  }
+  observer.observe(document, {childList: true, subtree: true})
 })()
