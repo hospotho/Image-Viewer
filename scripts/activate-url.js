74d910c1f7b15e9c9e696ef9618e31f4d1b017a1
hospotho <a155268747@gmail.com>
2023-02-19 01:50:56 +0800

    using more stable to load worker into iframe

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -34,19 +34,20 @@
     console.log('Init content script.')
     chrome.runtime.sendMessage('load_worker')
 
-    const observer = new MutationObserver(mutations => {
-      outer: for (const mutation of mutations) {
-        for (const node of mutation.addedNodes) {
-          if (node.nodeType === 1 && node.outerHTML.match('iframe')) {
-            console.log('New iframe')
-            chrome.runtime.sendMessage('load_worker')
-            break outer
-          }
+    const observer = new MutationObserver(() => {
+      const iframeList = document.querySelectorAll('iframe:not(.loadedWorker)')
+      if (iframeList.length) {
+        console.log('New iframe')
+        chrome.runtime.sendMessage('load_worker')
+        for (const iframe of iframeList) {
+          iframe.classList.add('loadedWorker')
         }
       }
     })
 
     observer.observe(document, {childList: true, subtree: true})
+    // for some rare case
+    setTimeout(() => chrome.runtime.sendMessage('load_worker'), 3000)
   }
 
   if (document.visibilityState === 'visible') {
