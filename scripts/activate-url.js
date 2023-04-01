c2b6a31ea0cdbeaf5298821cd2624bb8678ea0b5
hospotho <a155268747@gmail.com>
2023-04-02 04:32:42 +0800

    remove invalid iframe in init

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -15,17 +15,32 @@
   }
 
   const init = () => {
+    // chrome.scripting.executeScript never return on invalid iframe
+    const safeHref = 'about:blank'
+    const iframeList = document.querySelectorAll('iframe')
+    for (const iframe of iframeList) {
+      iframe.src = iframe.src.startsWith('javascript') ? safeHref : iframe.src
+      iframe.classList.add('loadedWorker')
+      iframe.classList.add('safeHref')
+    }
+
     console.log('Init content script.')
     chrome.runtime.sendMessage('load_worker')
 
     const observer = new MutationObserver(() => {
-      const iframeList = document.querySelectorAll('iframe:not(.loadedWorker)')
-      if (iframeList.length) {
-        console.log('New iframe')
-        chrome.runtime.sendMessage('load_worker')
-        for (const iframe of iframeList) {
-          iframe.classList.add('loadedWorker')
-        }
+      const newIframeList = document.querySelectorAll('iframe:not(.loadedWorker)')
+      if (newIframeList.length === 0) return
+
+      const iframeList = document.querySelectorAll('iframe:not(.safeHref)')
+      for (const iframe of iframeList) {
+        iframe.src = iframe.src.startsWith('javascript') ? safeHref : iframe.src
+        iframe.classList.add('safeHref')
+      }
+
+      console.log('New iframe')
+      chrome.runtime.sendMessage('load_worker')
+      for (const iframe of newIframeList) {
+        iframe.classList.add('loadedWorker')
       }
     })
 
