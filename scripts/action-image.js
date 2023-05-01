6e6a1816ae88eb69962d5b77fd4bbedc646632ad
hospotho <a155268747@gmail.com>
2023-05-01 11:36:21 +0800

    dom maybe null

--- scripts/action-image.js
+++ scripts/action-image.js
@@ -15,7 +15,7 @@
   const nodeInfo = await chrome.runtime.sendMessage('get_info')
   const [srcUrl, nodeSize] = nodeInfo === null ? [] : nodeInfo
   const dom = document.querySelector('.ImageViewerLastDom')
-  if (dom.tagName !== 'IMG') {
+  if (dom?.tagName !== 'IMG') {
     options.sizeCheck = true
   }
 
