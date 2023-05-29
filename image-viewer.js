9b570cff1b2e11c5c1f0135b22924eb8d439f126
hospotho <a155268747@gmail.com>
2023-05-30 00:46:36 +0800

    only observe once, remove IObserver option

--- image-viewer.js
+++ image-viewer.js
@@ -278,13 +278,7 @@ const imageViewer = (function () {
       }, 1000)
     }
 
-    const option = {
-      root: null,
-      rootMargin: '0px',
-      threshold: [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
-    }
-
-    const IObserver = new IntersectionObserver(action, option)
+    const IObserver = new IntersectionObserver(action)
     IObserver.observe(imgNode)
 
     const MObserver = new MutationObserver(() => {
