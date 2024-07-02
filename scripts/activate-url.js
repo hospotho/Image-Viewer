a6426b6632050aa01e3452658bbedd6414b9b679
hospotho <a155268747@gmail.com>
2024-07-02 18:15:14 +0800

    update content script to run in all frames

--- scripts/activate-url.js
+++ scripts/activate-url.js
@@ -7,115 +7,6 @@
     }
   }
 
-  let removalComplete = false
-
-  // normal web page mode
-  function isNonTrivialUrl(iframe) {
-    const src = iframe.src
-    if (src === '' || src === 'about:blank') {
-      iframe.classList.add('updateByTest')
-      return false
-    }
-    if (src.startsWith('javascript')) {
-      iframe.classList.add('updateByTest')
-      iframe.src = 'about:blank'
-      return false
-    }
-    if (iframe.loading !== 'eager') {
-      iframe.loading = 'eager'
-      iframe.classList.remove('loadedWorker')
-    }
-    return true
-  }
-  async function removeFailedIframe() {
-    for (const embed of document.getElementsByTagName('embed')) {
-      if (embed.type === '' || embed.type === 'text/html') {
-        embed.outerHTML = embed.outerHTML.replace(/^<embed(.*)>$/, '<iframe$1></iframe>')
-      } else {
-        embed.classList.add('loadedWorker')
-      }
-    }
-
-    const iframeList = [...document.querySelectorAll('iframe:not(.loadedWorker)')]
-    let found = false
-    for (const iframe of iframeList) {
-      iframe.classList.add('loadedWorker')
-      if (isNonTrivialUrl(iframe)) {
-        found = true
-        break
-      }
-    }
-    if (!found) {
-      removalComplete = true
-      return
-    }
-
-    const failedIframeList = (await safeSendMessage('check_frames')) || []
-    for (const src of failedIframeList) {
-      const targetList = iframeList.filter(iframe => iframe.src === src)
-      for (const iframe of targetList) {
-        if (iframe.src.startsWith('data')) continue
-        const newIframe = document.createElement('iframe')
-        newIframe.id = iframe.id
-        newIframe.className = iframe.className
-        newIframe.style = iframe.style
-        newIframe.width = iframe.width
-        newIframe.height = iframe.height
-        newIframe.loading = iframe.loading
-        newIframe.src = 'about:blank'
-        iframe.parentNode.replaceChild(newIframe, iframe)
-        console.log(`Remove failed iframe: ${src}`)
-      }
-    }
-    removalComplete = true
-  }
-  function initIframeObserver() {
-    const observer = new MutationObserver(async mutationList => {
-      let found = false
-      for (const mutation of mutationList) {
-        const target = mutation.target
-        if (target.tagName !== 'IFRAME') continue
-        if (target.classList.contains('updateByTest')) {
-          const {width, height} = target.getBoundingClientRect()
-          if (target.id === '' && target.className.replace('loadedWorker', '') === '' && width + height === 0) {
-            target.remove()
-            continue
-          }
-          target.classList.remove('updateByTest')
-          continue
-        }
-        found = true
-        target.classList.remove('loadedWorker')
-      }
-      if (!found && !document.querySelector('iframe:not(.loadedWorker)') && !document.querySelector('embed:not(.loadedWorker)')) return
-      if (removalComplete) {
-        removalComplete = false
-        await removeFailedIframe()
-        safeSendMessage('get_options')
-        safeSendMessage('load_worker')
-      }
-    })
-    observer.observe(document.documentElement, {childList: true, subtree: true, attributes: true, attributeFilter: ['src']})
-  }
-
-  async function initWorker() {
-    safeSendMessage('load_main_worker')
-
-    // chrome.scripting.executeScript never return on invalid iframe
-    initIframeObserver()
-    await removeFailedIframe()
-
-    safeSendMessage('get_options')
-    safeSendMessage('load_worker')
-
-    // for some rare case
-    setTimeout(async () => {
-      await removeFailedIframe()
-      safeSendMessage('get_options')
-      safeSendMessage('load_worker')
-    }, 3000)
-  }
-
   // image url mode
   function getRawUrl(src) {
     const argsRegex = /(.*?[=.](?:jpeg|jpg|png|gif|webp|bmp|tiff|avif))(?!\/)/i
@@ -184,18 +75,15 @@
   }
 
   async function init() {
-    await safeSendMessage('get_main_options')
+    await safeSendMessage('get_options')
     // Chrome terminated service worker
-    if (!window.ImageViewerOption) {
-      console.log('Wait service worker ready')
-    }
     while (!window.ImageViewerOption) {
       await new Promise(resolve => setTimeout(resolve, 50))
-      await safeSendMessage('get_main_options')
+      await safeSendMessage('get_options')
     }
 
     const image = document.querySelector(`img[src='${location.href}']`)
-    image ? initImageViewer(image) : initWorker()
+    image && window.top === window.self ? initImageViewer(image) : safeSendMessage('load_worker')
   }
 
   if (document.visibilityState === 'visible') {
