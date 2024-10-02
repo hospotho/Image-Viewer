8baea911f7f64a914bf2d17db16b4d01340f35b8
hospotho <a155268747@gmail.com>
2024-10-02 16:17:08 +0800

    change CORS check to sync

--- scripts/hook.js
+++ scripts/hook.js
@@ -72,15 +72,24 @@
     // return empty image on failure
     return result ? dataImage : new Image()
   }
+  function checkCORS(image) {
+    if (image.crossOrigin === 'anonymous') return false
+    const canvas = document.createElement('canvas')
+    canvas.width = 100
+    canvas.height = 100
+    const ctx = canvas.getContext('2d')
+    ctx.drawImage(image, 0, 0)
+    try {
+      canvas.toDataURL('image/png')
+      return false
+    } catch (error) {}
+    return true
+  }
 
   const realDrawImage = CanvasRenderingContext2D.prototype.drawImage
   CanvasRenderingContext2D.prototype.drawImage = async function (...args) {
     if (args[0] instanceof HTMLImageElement) {
-      if (this.canvas.cors === undefined) {
-        this.canvas.cors = await fetch(args[0].src)
-          .then(res => !res.ok)
-          .catch(() => true)
-      }
+      this.canvas.cors = this.canvas.cors || checkCORS(args[0])
       if (this.canvas.cors) {
         args[0] = await getBase64Image(args[0])
       }
