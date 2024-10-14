ebc771b0163f69ab1a3dc797827ab88500a4aab9
hospotho <a155268747@gmail.com>
2024-10-14 11:06:03 +0800

    add support page

---
+++ scripts/support.js
@@ -0,0 +1,27 @@
+;(function () {
+  'use strict'
+
+  chrome.i18n.getAcceptLanguages(languages => {
+    const exist = ['en', 'ja', 'zh_CN', 'zh_TW']
+    let displayLanguages = 'en'
+    for (const lang of languages) {
+      if (exist.includes(lang.replace('-', '_'))) {
+        displayLanguages = lang
+        break
+      }
+      if (exist.includes(lang.slice(0, 2))) {
+        displayLanguages = lang.slice(0, 2)
+        break
+      }
+    }
+    document.documentElement.setAttribute('lang', displayLanguages)
+  })
+
+  for (const el of document.querySelectorAll('[data-i18n]')) {
+    const tag = el.getAttribute('data-i18n')
+    const message = chrome.i18n.getMessage(tag)
+    if (!message) continue
+    el.textContent = message
+    if (el.value !== '') el.value = message
+  }
+})()
