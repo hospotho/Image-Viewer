86e86f062907704918657043074fc96a4454b7b4
hospotho <a155268747@gmail.com>
2026-05-06 22:13:47 +0800

    add chunked loading support for folder mode

--- scripts/action-folder.js
+++ scripts/action-folder.js
@@ -18,19 +18,30 @@
 
   // init
   const options = window.ImageViewerOption
+  const minSize = Math.min(options.minWidth, options.minHeight)
   options.closeButton = true
 
   // natural sort
   const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'})
   const anchorList = [...document.getElementsByTagName('a')].filter(a => !a.href.endsWith('/')).sort(collator.compare)
-  const sizeList = await Promise.all(anchorList.map(a => ImageViewerUtils.getImageRealSize(a.href)))
 
-  const imageDataList = []
-  const minSize = Math.min(options.minWidth, options.minHeight)
-  for (let i = 0; i < anchorList.length; i++) {
-    if (sizeList[i] >= minSize) imageDataList.push({src: anchorList[i].href, dom: anchorList[i]})
+  // load size in background
+  const asyncList = anchorList.map(a => a.getAttribute('iv-size') || ImageViewerUtils.getImageRealSize(a.href).then(size => a.setAttribute('iv-size', size)))
+
+  let ready = false
+  while (!ready) {
+    // collect image data
+    const imageDataList = []
+    for (const anchor of anchorList) {
+      const size = anchor.getAttribute('iv-size')
+      if (size && Number(size) >= minSize) imageDataList.push({src: anchor.href, dom: anchor})
+    }
+    // update image viewer
+    ImageViewer(imageDataList, options)
+    await new Promise(resolve => setTimeout(resolve, 100))
+
+    // check finish
+    const complete = await Promise.race([Promise.all(asyncList), new Promise(resolve => setTimeout(resolve, 0, false))])
+    if (complete) setTimeout(() => (ready = true), 100)
   }
-
-  // build image viewer
-  ImageViewer(imageDataList, options)
 })()
